89ee75ea667f8ed1e0c458943f45c42506c5d5eb
Mikael Cauã <mikaelcauasilva1@gmail.com>
2024-06-17 11:47:57 -0300

    init

---
+++ 2492_Minimum Score of a Path Between Two Cities.py
@@ -0,0 +1,7 @@
+from Grafo import Grafo
+
+grafo = Grafo()
+
+roads = [[1,2,9],[2,3,6],[2,4,5],[1,4,7]]
+n = 4
+
---
+++ Grafo.py
@@ -0,0 +1,18 @@
+class Grafo:
+    def __init__(self):
+        self.lista_adjacencia = {}
+
+    def adicionar_vertice(self, vertice):
+        if vertice not in self.lista_adjacencia:
+            self.lista_adjacencia[vertice] = []
+
+    def adicionar_aresta(self, vertice1, vertice2):
+        if vertice1 in self.lista_adjacencia and vertice2 in self.lista_adjacencia:
+            self.lista_adjacencia[vertice1].append(vertice2)
+            self.lista_adjacencia[vertice2].append(vertice1)  # Para grafos não direcionados
+
+    def mostrar_grafo(self):
+        for vertice in self.lista_adjacencia:
+            print(f'{vertice}: {self.lista_adjacencia[vertice]}')
+
+
